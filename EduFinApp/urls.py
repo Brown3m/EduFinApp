1442d171a3182deb916f0c19c8bd50239d162794
Brown3m <brown2004moses@gmail.com>
2026-03-11 21:11:46 +0200

    step 1 Lab2 finished

--- EduFinApp/urls.py
+++ EduFinApp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import testing_view,health_check
+from core.views import health_check, testing_detail_view, testing_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing', testing_view, name='testing'),
     path('health', health_check, name='health'),
+    path('testing/<int:id>', testing_detail_view, name='testing-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from core.models import Testing
+from core.serializers import TestingSerializer
+
+# Create your views here.
 
 def testing_view(request):
-	return JsonResponse({'messages': 'Hello World'})
+	data = Testing.objects.all()
+	serializer = TestingSerializer(data, many=True)
+	return JsonResponse(serializer.data, safe=False)
 
 def health_check(request):
 	return JsonResponse({'status': 'ok'})
 
+def testing_detail_view(request, id):
+	try:
+		data = Testing.objects.get(id=id)
+	except Testing.DoesNotExist:
+		return JsonResponse({'error': 'Record not found'}, status=404)
+
+	serializer = TestingSerializer(data)
+	return JsonResponse(serializer.data)
