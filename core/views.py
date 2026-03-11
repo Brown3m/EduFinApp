85e312c79d4d7bce316620b48ec2b5493ed3d5e2
Brown3m <brown2004moses@gmail.com>
2026-03-11 14:48:47 +0200

    initialize edufinapp django project in basic setup

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from core.models import Testing
+
+def testing_view(request):
+	return JsonResponse({'messages': 'Hello World'})
+
+def health_check(request):
+	return JsonResponse({'status': 'ok'})
+
